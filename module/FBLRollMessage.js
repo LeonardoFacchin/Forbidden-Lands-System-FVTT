2dcc1c92585026dc90fc577ed49813acf3cf34f3
Leonardo Facchin <68142386+LeonardoFacchin@users.noreply.github.com>
2020-08-18 00:36:44 +0200

    fixed chatRoll

--- module/FBLRollMessage.js
+++ module/FBLRollMessage.js
@@ -1,14 +1,14 @@
 export class FBLChatMessage extends ChatMessage {
 
-    constructor(data, options, fblRoll) {
+    constructor(data, options, fblRoll=null) {
         super(data, options);
         this.fblRoll = fblRoll || null;
     }
 
-    static async create(data, options, fblRoll) {
+    static async create(data, options, fblRoll=null) {
         let msg = await super.create(data, options);
         msg.fblRoll = fblRoll || null;
-        msg.fblRoll_id = fblRoll._id || null;
+        msg.fblRoll_id = fblRoll?._id || null;
         // console.log(msg);
         return msg;
     }
